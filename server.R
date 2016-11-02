bc2eab4b626152e53dbba18ca373fb0a79d2a74e
Daniel Hadley <dphnrome@gmail.com>
2016-11-02 12:14:57 -0600

    tweaking the example

---
+++ server.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(expm)
+
+# Based on http://shiny.rstudio.com/gallery/tabsets.html
+
+# Define server logic for random distribution application
+function(input, output) {
+  
+  # Reactive expression to generate the requested distribution.
+  # This is called whenever the inputs change. The output
+  # functions defined below then all use the value computed from
+  # this expression
+  data <- reactive({
+    dist <- rnorm
+    
+    dist(input$prison_sentence)
+  })
+  
+  # Generate a plot of the data. Also uses the inputs to build
+  # the plot label. Note that the dependencies on both the inputs
+  # and the data reactive expression are both tracked, and
+  # all expressions are called in the sequence implied by the
+  # dependency graph
+  output$plot <- renderPlot({
+    dist <- input$dist
+    n <- input$prison_sentence
+    
+    hist(data(), 
+         main=paste('r', dist, '(', n, ')', sep=''))
+  })
+  
+  # Generate a summary of the data
+  output$summary <- renderPrint({
+    summary(data())
+  })
+  
+  # Generate an HTML table view of the data
+  output$table <- renderTable({
+    data.frame(x=data())
+  })
+  
+}
---
+++ ui.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(scales)
+
+# Define UI for random distribution application 
+fluidPage(
+  
+  # Application title
+  titlePanel("Tabsets"),
+  
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the
+  # br() element to introduce extra vertical spacing
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("recid_rate", 
+                  "5-yr Recidivism Rate:", 
+                  value = .55,
+                  min = 0, 
+                  max = 1),
+      br(),
+      
+      sliderInput("prison_sentence", 
+                  "Average Prison Sentence (months):", 
+                  value = 31,
+                  min = 1, 
+                  max = 60),
+      
+      br(),
+      
+      sliderInput("cost_per_yr", 
+                  "Cost / Prisoner / Yr:", 
+                  value = 30000,
+                  min = 0, 
+                  max = 60000)
+    ),
+    
+    # Show a tabset that includes a plot, summary, and table view
+    # of the generated distribution
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Plot", plotOutput("plot")), 
+                  tabPanel("Summary", verbatimTextOutput("summary")), 
+                  tabPanel("Table", tableOutput("table"))
+      )
+    )
+  )
+)
