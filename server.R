8ea854014501b88de7a28b588fb8d0e66e58d2aa
sammnelson <sammatthewsnelson@gmail.com>
2016-12-05 00:13:50 -0700

    Should add a tab that has a riverplot. currently there are so many points showing that its hard to see whats happening. perhaps another reactive function that lets you choose which points to see?

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(expm)
 library(ggplot2)
+library(riverplot)
+library(RColorBrewer)
 
 default_data <- read.csv("./default_data.csv")
 
@@ -16,6 +18,8 @@ build_model <- function(recid_rate, prison_time_served, updateProgress = NULL){
   survival_rates <- matrix(c(recid*(recid_rate/.551), 1-(recid*(recid_rate/.551))), ncol = 2)
   
   number_in_prison <- rep(0,60)
+  number_released<-rep(0,60)
+  number_rearrested<-rep(0,60)
   
   # Made this a log normal becuse the long tail seems to capture the gap between
   # Mean and Median in actual BJS data
@@ -27,20 +31,26 @@ build_model <- function(recid_rate, prison_time_served, updateProgress = NULL){
     df <- data.frame(month=numeric(0),
                    months_free=numeric(0),
                    is_in_prison=numeric(0),
-                   rearrested=numeric(0))
+                   rearrested=numeric(0),
+                   released=numeric(0))
     for (month in 1:60){
       m.month<-month
       m.months_free<-calc_months_free(m.month,prison_time,tmp.months_free)
       m.rearrested<-calc_odds_of_being_rearrested(m.months_free,survival_rates)
       prison_time<-ifelse(m.rearrested==1,round(sample(prison_sample,1)),prison_time)
+      m.released<-ifelse(prison_time==1,1,0)
       m.is_in_prison<-say_if_in_prison(prison_time)
       prison_time<-ifelse(m.is_in_prison==1,prison_time-1,prison_time)
       tmp.months_free <- ifelse(m.rearrested == 1, 0, m.months_free)
-      df[month,]<-c(m.month,m.months_free,m.is_in_prison,m.rearrested)
+      df[month,]<-c(m.month,m.months_free,m.is_in_prison,m.rearrested,m.released)
       
     }
     single_agent_prison_time<-df$is_in_prison
+    released_by_month<-df$released
+    rearrested_by_month<-df$rearrested
     number_in_prison<-number_in_prison+as.numeric(single_agent_prison_time)
+    number_released<-number_released+as.numeric(released_by_month)
+    number_rearrested <-number_rearrested+as.numeric(rearrested_by_month)
     
     # If we were passed a progress update function, call it
     if (is.function(updateProgress)) {
@@ -49,7 +59,7 @@ build_model <- function(recid_rate, prison_time_served, updateProgress = NULL){
     }
     
   }
-  parolees<-data.frame(months=c(1:60),on_parole=1000-number_in_prison,prisoners=number_in_prison, survival_rates = survival_rates)
+  parolees<-data.frame(months=c(1:60),on_parole=1000-number_in_prison,prisoners=number_in_prison, survival_rates = survival_rates, released=number_released,rearrested=number_rearrested)
   
   
   
@@ -114,8 +124,8 @@ function(input, output) {
     # that it can update the progress indicator.
     rv$data <- build_model(input$recid_rate, input$prison_time_served, updateProgress)
   })
-  
-  # Generate a plot of the data. Also uses the inputs to build
+
+   Generate a plot of the data. Also uses the inputs to build
   output$plot <- renderPlot({
     ggplot(rv$data, aes(x = months, y = on_parole)) + geom_bar(stat = "identity")
   })
@@ -130,5 +140,36 @@ function(input, output) {
     data.frame(x=rv$data)
     
   })
+  
+  stayp<-data.frame(Value=data$on_parole)
+  staypr<-data.frame(Value=data$prisoners)
+  stayp<- stayp[-nrow(stayp),]
+  staypr<- staypr[-nrow(staypr),]
+  stay<-rbind(data.frame(Value=stayp),data.frame(Value=staypr))
+  
+  leavep<-data.frame(Value=data$rearrested)
+  leavepr<-data.frame(Value=data$released)
+  leavep<- leavep[-nrow(leavep),]
+  leavepr<- leavepr[-nrow(leavepr),]
+  leave<-rbind(data.frame(Value=leavep),data.frame(Value=leavepr))
+  
+  
+  
+  edges<-data.frame(N1 = paste0(rep(1:59, each = 1), rep(LETTERS[1:2], each = 59)),N2 = paste0(rep(2:60, each = 1), rep(LETTERS[1:2], each = 59)),Value=stay)
+  edges2<-data.frame(N1 = paste0(rep(1:59, each = 1), rep(LETTERS[1:2], each = 59)),N2 = paste0(rep(2:60, each = 1), rep(LETTERS[2:1], each = 59)),Value=leave)
+  edgesf<-rbind(edges,edges2)
+  
+  nodes<-data.frame(ID = paste0(rep(1:60, each = 1), rep(LETTERS[1:2], each = 60)),x = rep(1:60, each = 1),y=rep(2:1, each = 60))
+  rownames(nodes) = nodes$ID 
+  
+  palette = paste0(brewer.pal(4, "Set1"), "60")
+  styles = lapply(nodes$y, function(n) {list(col = palette[n+1], lty = 0, textcol = "black")})
+  names(styles) = nodes$ID
+  
+  river<-makeRiver(nodes,edgesf,edge_styles = styles)
+  
+  output$river<-renderPlot( plot(river))
+  
+  
   
 }
--- ui.R
+++ ui.R
@@ -49,7 +49,8 @@ fluidPage(
                            h2("About This App"),
                            p("Recidivism is one of society's most persistent, yet misunderstood, problems. Everyone from politicians to", a("Supreme Court Justices",     href= "https://www.themarshallproject.org/2014/12/04/the-misleading-math-of-recidivism#.AQSpHMFig"), "seem to get it wrong.")), 
                   tabPanel("Summary", verbatimTextOutput("summary")), 
-                  tabPanel("Table", tableOutput("table"))
+                  tabPanel("Table", tableOutput("table"),
+                  tabPanel("RiverPlot",plotOutput("river")))
       )
     )
   )
