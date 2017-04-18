71c57b06401ef784ca3d50b4a01137af5c213f61
Daniel Hadley <dphnrome@gmail.com>
2017-04-18 16:44:29 -0600

    replacing the shinydashboard which would not scroll with material design one

--- ui.R
+++ ui.R
@@ -4,24 +4,23 @@
 library(shiny)
 library(scales)
 library(plotly)
-library(shinydashboard)
+library(shinymaterial)
 # Define UI for random distribution application 
 
-dashboardPage(
-  dashboardHeader(title ="RecidiViz: Analyzing Reincarceration" ,
-                  titleWidth = 350),
-  dashboardSidebar(disable= TRUE),
-dashboardBody (
+
+
+material_page(
   
-  # Application title
-  
-  
+  title = "RecidiViz: Analyzing Reincarceration",
+
+
   # Sidebar with controls to select the random distribution type
   # and number of observations to generate. Note the use of the
   # br() element to introduce extra vertical spacing
-  fluidRow(
-    column(3,
-      h3("About this App"),
+  material_row(
+    material_column(
+      width = 3,
+      h5("About this App"),
       box(width=12, p("Recidivism is one of society's most persistent, yet misunderstood, problems. Everyone from politicians to", a("Supreme Court Justices",     href= "https://www.themarshallproject.org/2014/12/04/the-misleading-math-of-recidivism#.AQSpHMFig"), "seem to get it wrong.")),
       sliderInput("recid_rate", 
                   "5-yr Recidivism Rate:", 
@@ -56,16 +55,21 @@ dashboardBody (
       
     # Show a tabset that includes a plot, summary, and table view
     # of the generated distribution
-    column(9,
+    material_column(
+      width = 9,
       tabsetPanel(type = "tabs", 
                   tabPanel("Plot", 
-                           fluidRow(box(width=7, plotlyOutput("plot")),
-                                 box( width=5, htmlOutput("graph1"))),
-                           fluidRow(box(plotlyOutput("plot2")),
-                                    box(htmlOutput("graph2"))),
-                           fluidRow(box(plotlyOutput("plot3")),
-                                    box(htmlOutput("graph3")))),
+                           fluidRow(box(width=5, htmlOutput("graph1")),
+                                    box(width=7, plotlyOutput("plot"))
+                                    ),
+                           fluidRow(box(htmlOutput("graph2")),
+                                    box(plotlyOutput("plot2"))
+                                    ),
+                           fluidRow(box(htmlOutput("graph3")),
+                                    box(plotlyOutput("plot3"))
+                                    )
+                           ),
                   tabPanel("Data", tableOutput("table")))
       )
     )
-  ))
+  )
